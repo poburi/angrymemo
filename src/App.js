16d5e94ec8485c69ad6b1121637a15946496e058
hanheesong <shohom35@naver.com>
2020-07-14 18:40:03 +0900

    Hook-04: Click out side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React, {useRef, useEffect} from "react";
+import React, {useRef, useState, useEffect} from "react";
+function useClickOutside(fn){
+  const ref = useRef();
+  const handleClick = e => {
+    if(!ref.current.contains(e.target)){
+      fn();
+    }
+  }
+  useEffect(()=>{
+    document.addEventListener("click", handleClick)
+  }, [])
+  return ref;
+}
 
-/*
-* useRef는 전체 컴포넌트 중 하나의 node, element를 선택하는 방법이다.
-js의 document.getElementById()와 비슷하다고 생각하면 됨. 
-only one임.
-
-*/
 function App() {
-  const inputRef = useRef();
-  setTimeout(()=>{
-    inputRef.current.focus()
-  },3000)
+  const [close, setClose] = useState(true);
+
+  const onClickOutside = () => {
+    setClose(false);
+    console.log("lalala", close, ref);
+  }
+  const ref = useClickOutside(onClickOutside);
   return (
-    <div>
-      <div>
+    <div className="Wrapper">
+      <div ref={ref}>
         <h1>Hello Poburi</h1>
         <h2>This should be a popup</h2>
+        {!close?"close modal":"open modal"}
       </div>
-      <input type="text" ref={inputRef}/>
+      <input type="text"/>
     </div>
   )
 }
