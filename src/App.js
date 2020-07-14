09f9eab2e6bb3e06b901d57d3828551436181aa3
hanheesong <shohom35@naver.com>
2020-07-14 17:15:33 +0900

    Hook-03: useRef

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
-import React, {useState, useEffect} from "react";
-import Axios from "axios";
-import "./App.css";
+import React, {useRef, useEffect} from "react";
 
-function useInput(defaultValue){
-  const [value, setValue] = useState(defaultValue);
+/*
+* useRef는 전체 컴포넌트 중 하나의 node, element를 선택하는 방법이다.
+js의 document.getElementById()와 비슷하다고 생각하면 됨. 
+only one임.
 
-  const onChange = e => {
-    const {
-      target: {value}
-    } = e;
-    setValue(value);
-  };
-
-  return {value, onChange};
-}
-
-function useFectch(url){
-  const [payload, setPayload] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  const callUrl = async () => {
-    try {
-      const { data } = await Axios.get(url);
-      setPayload(data);
-    } catch {
-      setError("");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(()=>{
-    callUrl();
-  },[])
-
-  return {payload, loading, error};
-}
-
-
-const App = () => {
-  const name = useInput("");
-  const {payload, loading, error} = useFectch("http://aws.random.cat/meow");
+*/
+function App() {
+  const inputRef = useRef();
+  setTimeout(()=>{
+    inputRef.current.focus()
+  },3000)
   return (
     <div>
-      <h1>Use Hooks</h1>
-      {/* 
-        value = (name.value) onChange={name.onChange} === {...name}
-      */}
-      <input {...name} placeholder="성함을 입력해주세요." />
-      <br/>
-      {loading && <span>고양이를 불러오고 있습니다.</span>}
-      {!loading && error && <span>{error}</span>}
-      {!loading && payload && <img src={payload.file} width="150"/>}
+      <div>
+        <h1>Hello Poburi</h1>
+        <h2>This should be a popup</h2>
+      </div>
+      <input type="text" ref={inputRef}/>
     </div>
   )
-};
+}
 
 export default App;
