8748870c4c4ae8111c37bc7af62905c114df8f19
hanheesong <shohom35@naver.com>
2020-07-14 16:29:19 +0900

    hook-01

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# 리액트 이론
+# Hook
 
-- [SPA 라이프 사이클의 장점과 리액트](https://gmlthd0908.blog.me/221746568540)
-- [React의 컴포넌트 A-Z 그리고 주의사항!?](https://gmlthd0908.blog.me/221746719717)
-- [React, props와 state](https://gmlthd0908.blog.me/221746783613)
-- [React, prop(children, method, object)](https://blog.naver.com/gmlthd0908/221747922862)
+- useTitle
+- useInput
+- usePageLeave
+- useClick
+- useFadeIn
+- useFullScreen
+- useHover
+- useNetwork
+- useNotification
+- useScroll
+- useTabs
+- usePreventLeave
+- useConfirm
+- useAxios
+
+
+# useEffect
+
+```
+useEffect(()=>{
+
+}.[])
+```
+
--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useState} from "react";
 import "./App.css";
-import "./Person.css";
-import { Person } from "./Person";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(props.test);
-    this.state = {
-      person: [
-        { name: "buribooth", age: 27, myfunc: () => console.log("test") },
-        { name: "연개소문", age: 64 },
-        { name: "대조영", age: 26 }
-      ]
-    };
+const App = () => {
+  const [email, setEmail] = useState("");
+  const updateEmail = e => {
+    const {
+      target: {value}
+    } = e;
+    setEmail(value);
   }
-  render() {
-    const { person } = this.state;
-    const myfunc = () => {
-      console.log("진짜진짜???");
-    };
-    return (
-      <div className="App">
-        <h1>{this.props.test}</h1>
-        <Person {...person[0]} myfunc={myfunc} st={this.state}>
-          <div>안녕하세요!</div>
-        </Person>
-      </div>
-    );
-  }
-}
+
+  return (
+    <section>
+      <input placeholder="Email" value={email} onChange={updateEmail}/>
+    </section>
+  );
+};
 
 export default App;
